1963a403afaa08d8a1342defce9b5336b4c9600d
something7833 <23cse522.ansumanmahapatra@giet.edu>
2025-08-03 17:48:59 +0530

    Added MyFirstGitProject

---
+++ MyFirstGitProject.java
@@ -0,0 +1,11 @@
+package Git_Practice;
+import java.util.*;
+
+public class MyFirstGitProject {
+    public static void main(String[] args) {
+
+        System.out.println("Git_Practice");
+
+
+    }
+}
